71d5cce37a8ab8dfaa1d4b2187f564dea94d0a23
axon4 <65050809+axon4@users.noreply.github.com>
2024-01-21 19:08:39 +0000

    replace footer with pertinent-verse--link

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import { Footer as ArwesFooter, Paragraph } from 'arwes';
+import { Footer as ArwesFooter, Link } from 'arwes';
 import Centred from './Centred';
 
 function Footer() {
 	return (
 		<ArwesFooter animate>
 			<Centred>
-				<Paragraph style={{fontSize: 14, margin: '10px 0'}}>
-					This Is Not An Official Site And Is Not Affiliated With NASA or SpaceX In Any Way. For Educational Purposes Only
-				</Paragraph>
+				<Link href='https://quran.com/55/33' style={{fontSize: 14, margin: '10px 0', display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'underline', textUnderlineOffset: '5px'}} target='_blank'>
+					Sūraṫ ar-Raḥmān 55, Ayah 33 | 55:33
+				</Link>
 			</Centred>
 		</ArwesFooter>
 	);
