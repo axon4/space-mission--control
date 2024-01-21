5fcd6b42cd917b931aedadc2afaa173def1f2631
axon4 <65050809+axon4@users.noreply.github.com>
2024-01-21 10:01:21 +0000

    add scripts to build client and serve from server

--- server/Express.js
+++ server/Express.js
@@ -1,14 +1,18 @@
+const path = require('path');
 const Express = require('express');
 const CORS = require('cors');
 const planetsRouter = require('./routers/planets/planetsRouter');
 
 const server = Express();
 
-server.use(CORS({
-	origin: 'http://localhost:3000'
-}));
+server.use(CORS({origin: 'http://localhost:3000'}));
 server.use(Express.json());
+server.use(Express.static(path.join(__dirname, 'dist')));
 
 server.use(planetsRouter);
+
+server.get('/', (_request, response) => {
+	response.status(200).sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 module.exports = server;
