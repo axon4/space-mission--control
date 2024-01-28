94a86afd3a0b81eacd7c61302bba01d25a7aba6f
axon4 <65050809+axon4@users.noreply.github.com>
2024-01-28 09:24:15 +0000

    connect to MongoDB-dataBase with MonGoose

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const HTTP = require('http');
+const MonGoose = require('mongoose');
 const Express = require('./Express');
 const { loadPlanets } = require('./models/planetsModel');
 
 const PORT = process.env.PORT || 3001;
+const { MONGO_CONNECTION_STRING } = process.env;
 
 const server = HTTP.createServer(Express);
 
-Promise.all([loadPlanets()])
+MonGoose.connection.once('open', () => {console.log('Mongo connected')});
+
+MonGoose.connection.on('error', error => {console.error(error)});
+
+Promise.all([
+	MonGoose.connect(MONGO_CONNECTION_STRING, {
+		// useNewUrlParser: true,
+		// useFindAndModify: false,
+		// useCreateIndex: true,
+		// useUnifiedTopology: true
+	}),
+	loadPlanets()
+])
 	.then(() => {
 		server.listen(PORT, () => {
 			console.log(`listening on port: ${PORT}`);
